21291e4cf0fcee84e452168b3d3b50e13fc8abe7
kulho <jakubkulhanek82103@gmail.com>
2023-05-08 21:48:27 +0200

    token creation and support for multichain bridge

---
+++ contracts/DLTPayToken-Ethereum.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract DLTPayToken is ERC20, ERC20Burnable, Pausable, AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MULTICHAIN_BRIDGE_ROLE =
+        keccak256("MULTICHAIN_BRIDGE_ROLE");
+
+    // ---------------------------------------------------------------- //
+    // Support for anyswap/multichain.org
+    // according to https://docs.multichain.org/developer-guide/how-to-develop-under-anyswap-erc20-standards
+    // and https://github.com/anyswap/chaindata/blob/main/AnyswapV6ERC20.sol
+    address public immutable underlying;
+
+    event LogSwapin(
+        bytes32 indexed txhash,
+        address indexed account,
+        uint amount
+    );
+    event LogSwapout(
+        address indexed account,
+        address indexed bindaddr,
+        uint amount
+    );
+
+    function mint(
+        address to,
+        uint256 amount
+    ) public onlyRole(MULTICHAIN_BRIDGE_ROLE) returns (bool) {
+        _mint(to, amount);
+        return true;
+    }
+
+    function burn(
+        address from,
+        uint256 amount
+    ) external onlyRole(MULTICHAIN_BRIDGE_ROLE) returns (bool) {
+        _burn(from, amount);
+        return true;
+    }
+
+    // For backwards compatibility
+    function Swapin(
+        bytes32 txhash,
+        address account,
+        uint256 amount
+    ) external onlyRole(MULTICHAIN_BRIDGE_ROLE) returns (bool) {
+        _mint(account, amount);
+        emit LogSwapin(txhash, account, amount);
+        return true;
+    }
+
+    // For backwards compatibility
+    function Swapout(uint256 amount, address bindaddr) external returns (bool) {
+        require(bindaddr != address(0), "AnyswapV6ERC20: address(0)");
+        _burn(msg.sender, amount);
+        emit LogSwapout(msg.sender, bindaddr, amount);
+        return true;
+    }
+
+    // ---------------------------------------------------------------- //
+
+    constructor() ERC20("DLTPAY", "DLTPAY") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        underlying = address(0);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
